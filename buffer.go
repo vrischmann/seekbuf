4603ff20bfa6b4cb1f89241e316f9c53e9f252db
Vincent Rischmann <me@vrischmann.me>
2016-05-10 01:31:58 +0200

    initial commit

---
+++ buffer.go
@@ -0,0 +1,77 @@
+package seekbuf
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+type Buffer struct {
+	data []byte
+	pos  int
+}
+
+// Bytes returns a slice holding the data from the current position up to the end.
+func (b *Buffer) Bytes() []byte {
+	return b.data[b.pos:]
+}
+
+// Read reads the next len(p) bytes from the buffer starting from the current position.
+//
+func (b *Buffer) Read(p []byte) (int, error) {
+	if len(p) == 0 {
+		return 0, nil
+	}
+
+	if len(b.data[b.pos:]) <= 0 {
+		return 0, io.EOF
+	}
+
+	n := copy(p, b.data[b.pos:])
+	b.pos += n
+
+	return n, nil
+}
+
+// Write the contents of p in the buffer at the current position.
+func (b *Buffer) Write(p []byte) (int, error) {
+	n := copy(b.data[b.pos:], p)
+	b.data = append(b.data, p[n:]...)
+	b.pos += len(p)
+
+	return len(p), nil
+}
+
+// Seek sets the offset for the next Read or Write on the buffer to offset, interpreted according to whence:
+// 0 means relative to the origin of the file, 1 means relative to the current offset,
+// and 2 means relative to the end.
+// It returns the new offset and an error, if any.
+func (b *Buffer) Seek(offset int64, whence int) (int64, error) {
+	o := int(offset)
+	switch whence {
+	case os.SEEK_CUR:
+		if o > 0 && b.pos+o >= len(b.data) {
+			return -1, fmt.Errorf("invalid offset %d", offset)
+		}
+		b.pos += o
+
+	case os.SEEK_SET:
+		if o > 0 && o >= len(b.data) {
+			return -1, fmt.Errorf("invalid offset %d", offset)
+		}
+		b.pos = o
+
+	case os.SEEK_END:
+		if len(b.data)-o < 0 {
+			return -1, fmt.Errorf("invalid offset %d", offset)
+		}
+		b.pos = len(b.data) - o
+
+	default:
+		return -1, fmt.Errorf("invalid whence %d", whence)
+	}
+
+	return int64(b.pos), nil
+}
+
+var _ io.ReadWriteSeeker = (*Buffer)(nil)
---
+++ buffer_test.go
@@ -0,0 +1,187 @@
+package seekbuf_test
+
+import (
+	"io"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/vrischmann/seekbuf"
+)
+
+type readWriteTestCase struct {
+	input   string
+	output  string
+	expWErr string
+	expRErr string
+}
+
+var readWriteTestCases = []readWriteTestCase{
+	{"foobar", "foobar", "", ""},
+	{"", "foo", "", "EOF"},
+}
+
+func TestReadWrite(t *testing.T) {
+	var b seekbuf.Buffer
+
+	n, err := b.Write([]byte("foobar"))
+	require.Nil(t, err)
+	require.Equal(t, 6, n)
+
+	b.Seek(0, os.SEEK_SET)
+
+	var data [6]byte
+	n, err = b.Read(data[:])
+	require.Nil(t, err)
+	require.Equal(t, 6, n)
+	require.Equal(t, "foobar", string(data[:]))
+}
+
+func TestReadWriteBiggerReadBuf(t *testing.T) {
+	var b seekbuf.Buffer
+
+	n, err := b.Write([]byte("foo"))
+	require.Nil(t, err)
+	require.Equal(t, 3, n)
+
+	b.Seek(0, os.SEEK_SET)
+
+	var data [6]byte
+	n, err = b.Read(data[:])
+	require.Nil(t, err)
+	require.Equal(t, 3, n)
+	require.Equal(t, "foo", string(data[:n]))
+}
+
+func TestReadWriteEOF(t *testing.T) {
+	var b seekbuf.Buffer
+
+	var data [6]byte
+	n, err := b.Read(data[:])
+	require.NotNil(t, err)
+	require.Equal(t, 0, n)
+	require.Equal(t, io.EOF, err)
+}
+
+func TestReadWriteSeekSet(t *testing.T) {
+	var b seekbuf.Buffer
+
+	n, err := b.Write([]byte("foobar"))
+	require.Nil(t, err)
+	require.Equal(t, 6, n)
+
+	b.Seek(0, os.SEEK_SET)
+
+	var data [6]byte
+	n, err = b.Read(data[:])
+	require.Nil(t, err)
+	require.Equal(t, 6, n)
+	require.Equal(t, "foobar", string(data[:]))
+
+	n2, err := b.Seek(0, os.SEEK_SET)
+	require.Nil(t, err)
+	require.Equal(t, int64(0), n2)
+
+	n, err = b.Read(data[:])
+	require.Nil(t, err)
+	require.Equal(t, 6, n)
+	require.Equal(t, "foobar", string(data[:]))
+}
+
+func TestReadWriteSeekCur(t *testing.T) {
+	var b seekbuf.Buffer
+
+	n, err := b.Write([]byte("foobar"))
+	require.Nil(t, err)
+	require.Equal(t, 6, n)
+
+	n2, err := b.Seek(3, os.SEEK_SET)
+	require.Nil(t, err)
+	require.Equal(t, int64(3), n2)
+
+	require.Equal(t, 3, len(b.Bytes()))
+	require.Equal(t, "bar", string(b.Bytes()))
+
+	n2, err = b.Seek(1, os.SEEK_CUR)
+	require.Nil(t, err)
+	require.Equal(t, int64(4), n2)
+
+	require.Equal(t, 2, len(b.Bytes()))
+	require.Equal(t, "ar", string(b.Bytes()))
+}
+
+func TestReadWriteSeekEnd(t *testing.T) {
+	var b seekbuf.Buffer
+
+	n, err := b.Write([]byte("foobar"))
+	require.Nil(t, err)
+	require.Equal(t, 6, n)
+
+	n2, err := b.Seek(1, os.SEEK_END)
+	require.Nil(t, err)
+	require.Equal(t, int64(5), n2)
+
+	require.Equal(t, 1, len(b.Bytes()))
+	require.Equal(t, "r", string(b.Bytes()))
+}
+
+func TestSeekEmpty(t *testing.T) {
+	var b seekbuf.Buffer
+
+	n, err := b.Seek(0, os.SEEK_SET)
+	require.Nil(t, err)
+	require.Equal(t, int64(0), n)
+
+	n, err = b.Seek(0, os.SEEK_CUR)
+	require.Nil(t, err)
+	require.Equal(t, int64(0), n)
+
+	n, err = b.Seek(0, os.SEEK_END)
+	require.Nil(t, err)
+	require.Equal(t, int64(0), n)
+}
+
+func TestSeekZeroNotEmpty(t *testing.T) {
+	var b seekbuf.Buffer
+
+	b.Write([]byte("foobar"))
+
+	n, err := b.Seek(0, os.SEEK_SET)
+	require.Nil(t, err)
+	require.Equal(t, int64(0), n)
+
+	n, err = b.Seek(0, os.SEEK_CUR)
+	require.Nil(t, err)
+	require.Equal(t, int64(0), n)
+
+	n, err = b.Seek(0, os.SEEK_END)
+	require.Nil(t, err)
+	require.Equal(t, int64(6), n)
+}
+
+func TestSeekOverwrite(t *testing.T) {
+	var b seekbuf.Buffer
+
+	b.Write([]byte("foobar"))
+	b.Seek(0, os.SEEK_SET)
+	b.Write([]byte("baz"))
+
+	b.Seek(0, os.SEEK_SET)
+
+	require.Equal(t, "bazbar", string(b.Bytes()))
+}
+
+func TestWrite(t *testing.T) {
+	var b seekbuf.Buffer
+
+	n, err := b.Write([]byte("foobar"))
+	require.Nil(t, err)
+	require.Equal(t, 6, n)
+	n, err = b.Write([]byte("quxbaz"))
+	require.Nil(t, err)
+	require.Equal(t, 6, n)
+
+	b.Seek(0, os.SEEK_SET)
+
+	require.Equal(t, "foobarquxbaz", string(b.Bytes()))
+}
